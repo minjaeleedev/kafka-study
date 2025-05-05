f967ca37360d27c2fa575e68f2b5fd46a63bbae6
minjaelee <minjaeleedev@gmail.com>
2025-05-05 19:53:48 +0900

    feat: synchronous producer

---
+++ ch3-producer/src/main/java/com/example/kafkaproducer/SynchronousRawKafkaProducer.java
@@ -0,0 +1,39 @@
+package com.example.kafkaproducer;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PreDestroy;
+import java.util.Properties;
+
+@Component
+public class SynchronousRawKafkaProducer {
+    private final KafkaProducer<String, String> producer;
+
+    public SynchronousRawKafkaProducer() {
+        Properties kafkaProps = new Properties();
+        kafkaProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        this.producer = new KafkaProducer<>(kafkaProps);
+    }
+
+    public void send(String topic, String key, String message) {
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, key, message);
+        try {
+          // send synchronous
+          producer.send(record).get();
+        } catch(Exception e) {
+          e.printStackTrace();
+        }
+    }
+
+    @PreDestroy
+    public void close() {
+        producer.close();
+    }
+}
